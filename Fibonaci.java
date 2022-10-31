b2c06c33c22ce521d2abdc401e1d70fe83128207
fatvoltage777 <66829941+fatvoltage777@users.noreply.github.com>
2022-11-01 00:48:15 +1100

    Add files via upload

---
+++ Fibonaci.java
@@ -0,0 +1,20 @@
+
+public class Fibonaci {
+	public static void main(String[] args) {
+		int num = 4;
+		int fiboNum = findFibo(num);
+		System.out.println(fiboNum);
+	}
+
+	public static int findFibo(int num) {
+
+		int[] result = new int[num + 2];
+		result[0] = 0;
+		result[1] = 1;
+
+		for (int i = 2; i <= num; i++) {
+			result[i] = result[i - 1] + result[i - 2];
+		}
+		return result[num];
+	}
+}
